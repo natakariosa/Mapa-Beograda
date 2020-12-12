7ae3a36c70150af7229016a2de0bcf056cc70e2a
natakariosa <72417476+natakariosa@users.noreply.github.com>
2020-12-12 12:14:27 +0100

    Add files via upload

--- projekat.c
+++ projekat.c
@@ -1,21 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "projekat.h"
 
 FILE *otvori( char *ime , char *kako )
 {
     FILE *d = fopen( ime , kako );
-    
+
     if( d == NULL ) exit( EXIT_FAILURE );
-    
+
     return( d );
 }
 
 Graf *napravi_element( int vrednost , double tezina , char *ime , int br_ulice )
 {
     Graf *novi = ( Graf * ) malloc( sizeof( Graf ) );
-    
+
     strcpy( novi->ime , ime );
     novi->br_ulice = br_ulice;
-    
+
     novi->id = vrednost;
     novi->tezina = tezina;
     novi->put = INFINITY;
@@ -39,9 +41,9 @@ void dodaj_ivicu( Graf **glava , double vrednost , double tezina , char *ime ) /
     else
     {
         pom = *glava;
-        
+
         while( pom->sledeci ) pom = pom->sledeci;
-        
+
         if( strcmp( ime , "Nema naziv" ) == 0 ) pom->sledeci = napravi_element( vrednost, tezina , ime , 0 );
         else pom->sledeci = napravi_element( vrednost, tezina , ime , pom->br_ulice + 1 );
     };
@@ -50,7 +52,7 @@ void dodaj_ivicu( Graf **glava , double vrednost , double tezina , char *ime ) /
 int prazna_linija( char *linija )
 {
     int i;
-    
+
     for( i = 0 ; linija[i] != '\0'; ++i ) if( !isspace( linija[i] ) ) return( 0 );
 
     return( 1 );
@@ -60,23 +62,23 @@ char **napravi_niz( char **niz , int dim )
 {
     int i = 0 , j;
     char ime[50];
-    
+
     FILE *dat = otvori( "pevac.pypgr_names", "r" );
-   
+
     niz = ( char ** ) malloc( sizeof( char * ) * dim );
-    
+
     for( j = 0 ; j < dim ; j++ ) niz[j] = ( char * ) malloc( sizeof( char ) * 150 );
-    
+
     while( fgets( ime , 2*sizeof( ime ) , dat ) )
     {
         if( prazna_linija( ime ) ) strcpy( niz[i] , "Nema naziv\n" );
         else strcpy( niz[i] , ime );
- 
+
         i++;
     };
-    
+
     fclose( dat );
-    
+
     return( niz );
 }
 
@@ -98,22 +100,22 @@ Graf **napravi_graf( Graf **G , int *dim1 )
       printf( "Greska pri alokaciji memorije!\n" );
       exit( EXIT_FAILURE );
     };
-    
+
     for( i = 0 ; i < *dim1 ; i++ )
     {
         G[i] = ( Graf * ) malloc( sizeof( Graf ) );
-      
+
         fscanf( dat1 , "%d %lf %lf", &G[i]->id , &G[i]->longituda, &G[i]->latituda );
     };
-    
+
     niz = napravi_niz( niz , dim2 );
-    
+
     for( i = 0 ; i < dim2 ; i++ )
     {
         fscanf( dat1 , "%d %d %lf %s %d %d" , &pocetni , &krajnji , &duzina , pom , &brzina , &jednosmerna );
-        
+
         dodaj_ivicu( &( G[pocetni] ) , krajnji , duzina , niz[i] );
-            
+
         if( jednosmerna == 0 ) dodaj_ivicu( &( G[krajnji] ) , pocetni , duzina , niz[i] );
     };
 
@@ -127,17 +129,17 @@ void stampaj( Graf **G, Brojaci dim )
     Brojaci i;
 
     printf( "\n" );
-    
+
     for( i = 0; i < dim ; i++ )
     {
         printf( "Lista povezanosti za cvor %d: \n" , i );
-        
+
         while( *( G + i ) != NULL )
         {
             printf( "%d[%lf][%s %d] -> ", ( *( G + i ) )->id, ( *( G + i ) )->tezina , ( *( G + i ) )->ime , ( *( G + i ) )->br_ulice );
             *( G + i ) = (*( G + i ))->sledeci;
         };
-        
+
         printf( "Kraj liste\n" );
     };
 }
