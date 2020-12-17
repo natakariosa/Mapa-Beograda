908cba054a8ae2a0ad877e6b6b98d7cca760ad2a
Lazar Lakovic <laza.vb@gmail.com>
2020-12-17 12:18:44 +0100

    Izmena

---
+++ NOVI_projekat/projekat_main.c
@@ -0,0 +1,12 @@
+#include "projekat.h"
+
+int main( void )
+{
+    Graf *G = napravi_graf();
+    int i=0;
+    
+//     for( i = 0 ; i < G->dim ; i++ ) printf( "%ld %lf %lf\n" , G->putevi[i]->id , G->putevi[i]->lat , G->putevi[i]->lon );
+    
+    
+    return( 0 );
+}
