33e95d62d5035aad56012cda12a73c1403fb0dde
Lazar Lakovic <laza.vb@gmail.com>
2020-12-23 00:36:28 +0100

    Test

--- Uradjen_graf/projekat_main.c
+++ Uradjen_graf/projekat_main.c
@@ -1,4 +1,5 @@
 #include "projekat.h"
+/*Argumenti za test primer: 3055122589 2895159338*/
 
 int main( int argc, char **argv ) //kompajlovati sa: gcc projekat_main.c projekat.c projekat.h -o a.out -lm
 {
